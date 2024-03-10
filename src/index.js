ebf7b8714c994a21861649177e3aafca8ee110c1
ashleyruizca <156567181+ashleyruizca@users.noreply.github.com>
2024-03-09 22:38:22 -0500

    added a line under the days

--- src/index.js
+++ src/index.js
@@ -77,7 +77,7 @@ function displayForecast(response) {
         `
       <div class="weather-forecast-day">
         <div class="weather-forecast-date">${formatDay(day.time)}</div>
-
+<hr/>
         <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
         <div class="weather-forecast-temperatures">
           <div class="weather-forecast-temperature">
