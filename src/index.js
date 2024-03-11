4d2370aad8d139aaf48af3f45556c49ae4062088
ashleyruizca <156567181+ashleyruizca@users.noreply.github.com>
2024-03-10 20:37:01 -0400

    added lines

--- src/index.js
+++ src/index.js
@@ -75,9 +75,9 @@ function displayForecast(response) {
       forecastHtml =
         forecastHtml +
         `
+        <hr/>
       <div class="weather-forecast-day">
         <div class="weather-forecast-date">${formatDay(day.time)}</div>
-<hr/>
         <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
         <div class="weather-forecast-temperatures">
           <div class="weather-forecast-temperature">
